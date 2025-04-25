d20592f0af7280952eaa7f8e7c9543d518b4a531
wndudwns0028 <yeong9701@gmail.com>
2025-04-25 15:18:21 +0900

    ip add

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 # 만약 여러 도메인을 허용하거나 테스트용이라면 ["*"]로 변경 가능
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
